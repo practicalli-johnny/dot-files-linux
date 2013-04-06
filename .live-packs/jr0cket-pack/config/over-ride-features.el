ad6f1e5446decfe9a2066bef229c54b19c8d2ea0
John Stevenson <john@jr0cket.co.uk>
2013-04-06 12:46:36 +0100

    deleted old file for emacs feature override

--- .live-packs/jr0cket-pack/config/over-ride-features.el
+++
@@ -1,8 +0,0 @@
-;; Overriding emacs-live features
-
-;; live-whitespace-cleanup removes blank space at the end of lines and empty lines at the start or end of the buffer. To remove that behaviour add the following to your onw live-pack configuration:
-
-; (remove-hook 'before-save-hook 'live-whitespace-cleanup)
-
-
-;; Zoning
