22d3f69bde2e4a78c4b8e894ee243208f0b827a5
John Stevenson <john@jr0cket.co.uk>
2013-04-09 15:03:42 +0100

    fixed feature switch-off for live cleanup whitespace, function name was not quite right

--- .live-packs/jr0cket-pack/config/override-emacs-live-features.el
+++ .live-packs/jr0cket-pack/config/override-emacs-live-features.el
@@ -1,9 +1,13 @@
 ;; Overriding emacs-live features
 
-;; live-whitespace-cleanup
+;; live-cleanup-whitespace
 ;; removes blank space at the end of lines and empty lines at the start or end of the buffer. To remove that behaviour add the following to your onw live-pack configuration:
 
-; (remove-hook 'before-save-hook 'live-whitespace-cleanup)
+; Sam's refinement on whitespace removal - a much more reserved cleanup fn which won't mess about with tabs and spaces at the beginning of a line and only remove blank space at the end of lines and empty lines at the start or end of the buffer which will replace whitespace-cleanup in the next Emacs Live beta
+;;; (add-hook 'before-save-hook 'live-cleanup-whitespace)
+
+(remove-hook 'before-save-hook 'live-cleanup-whitespace)
+
 
 
 ;; Zoning
