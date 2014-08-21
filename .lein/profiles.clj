db472ed8e72176c09136ee83b2dea5aa85ef4506
John Stevenson <john@jr0cket.co.uk>
2014-08-21 22:23:17 +0100

    Added leiningen lighttable plugin to profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-difftest "1.3.8"]
                   [lein-marginalia "0.7.1"]
-                  [lein-pprint "1.1.1"]]}}
+                  [lein-pprint "1.1.1"]
+                  [lein-light "0.0.4"]]}}
