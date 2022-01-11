95bed352bbd8891adee0a5db24c5179e695ec319
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-01-11 19:41:21 +0000

    remove Emacs live custom configuration

--- .live-packs/jr0cket-pack/config/emacs-server.el
+++
@@ -1,7 +0,0 @@
-;; Start the emacs server so that emacs client can be used
-;; to quickly edit files
-
-;; A check is done to ensure the emacs server is not already running.
-
-(load "server")
-(unless (server-running-p) (server-start))
--- .live-packs/jr0cket-pack/config/emms.el
+++
@@ -1,5 +0,0 @@
-;; Emacs multi-media system setup
-
-(require 'emms-setup)
-(emms-standard)
-(emms-default-players)
